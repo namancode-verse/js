d99673280b3b885c85c922d8e1bf5833f4f4dc75
namancode-verse <138519195+namancode-verse@users.noreply.github.com>
2024-02-28 08:30:40 +0000

    data

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=144354
+let accountsEmail="naman@google.com"
+var accountPassword= "12345"
+accountCity="jaipur"
+// accountId=2    ,its not allowed to change value of this
+console.log(accountId);
+accountEmail="naman#nnd.com"
+accountCity="bengaluru"
+let accountState;
+console.table([accountEmail,accountCity,accountId,accountState])
+/* prefer not to use var */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js codes as newer version bec earlier many things were missing in js unlike today
+
+// alert("hello")
+// alert(2+4) //we are using node js ,not browser
+
+console.log(3 +3)
+console.log("hitesh")
+
+let name="naman"
+let age=38
+let isLoggedIn=false
+
+//number ==> 2 to power 53
+//  big ==> for more range
+//string ==> ""
+// boolean ==> true/false
+//null ==> its a datatype also in js and we can assign null also bec it means empty
+//undefined is when no value is defined;
+// symbol ==>unique
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
